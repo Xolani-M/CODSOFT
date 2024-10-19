55fe47bff40aadd61db6a0433a808c80e73d2828
sageMode <nathanielmvana@gmail.com>
2024-10-19 20:38:28 +0200

    feat: Added GameMessenger class for centralized game messaging

---
+++ task_1_number_game/src/GameMessenger.java
@@ -0,0 +1,15 @@
+package task_1_number_game.src;
+
+public class GameMessenger {
+
+    public static final String WELCOME_MSG = "Welcome to the Number Guessing Game!";
+    public static final String NEW_GAME_MSG = "New Game: Guess a number between %d and %d%n";
+    public static final String GUESS_LOW_MSG = " is too low!";
+    public static final String GUESS_HIGH_MSG = " is too high!";
+    public static final String FLIP_COIN_MSG = "Flip a coin... or just guess the target";
+    public static final String GAME_OVER_MSG = "Game over! You've reached the maximum attempts. The target was %d.";
+    public static final String CORRECT_GUESS_MSG = "Correct! You nailed it in %d attempts! Your score is %d.";
+    public static final String INVALID_INPUT_MSG = "Coin toss didn't go your way. Enter a valid integer.";
+    public static final String PLAY_AGAIN_PROMPT = "Do you want to play again? (yes/no): ";
+
+}
